8815efaf0231da8b2edb771298a0f9980ea70324
Ryanna Luo <ryanna668@gmail.com>
2021-10-19 09:36:43 -0400

    pastel drawing program

--- src/drawing/FirstDrawing.java
+++ src/drawing/FirstDrawing.java
@@ -23,44 +23,86 @@ public class FirstDrawing {
 	//Global variables
 	int panW = 800, panH = 800;
 	
+
 	FirstDrawing(){
 		//setup JFrame
-		JFrame window = new JFrame("Drawing on JPanel");		
+		JFrame window = new JFrame("Welcome To My Pastel Palette :)");		
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+
 		DrawingPanel panel = new DrawingPanel();
 		window.add(panel);
 		window.pack(); 		//sets optimum size
-		window.setLocationRelativeTo(null);  //centre on screen
+		window.setLocationRelativeTo(null);  //center on screen
 		window.setVisible(true);
 	}
-	
+
 	class DrawingPanel extends JPanel {
-		
+
 		DrawingPanel() {
 			//this controls the size ***
 			this.setPreferredSize(new Dimension(panW, panH));
-			this.setBackground(Color.decode("#221177"));
+			this.setBackground(Color.decode("#F4F4F4"));
 		}
-		
+
 		//all drawing goes in here
 		@Override
 		public void paintComponent(Graphics g) {
 			super.paintComponent(g); //clear and set BGcolor
-			g.setColor(Color.YELLOW);
-			
-			g.drawOval(150, 10, 100,100);
+
 			//turn on antialias
 			Graphics2D g2 = (Graphics2D) g; 
 			g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-					
-			g2.drawOval(10, 10, 100,100);
-			//how to make a thicker line
+
+			//dark green
+			g2.setColor(Color.decode("#557571"));
+			g2.drawRect (0, 600, 800, 100);
+			g2.fillRect (0, 600, 800, 100);
+
+			g2.drawOval(725, 20, 50,50);
+
 			g2.setStroke(new BasicStroke(4));
-			//why does this work if I use g and not g2?
-			g.drawLine(0,300,100,300);
-			
+			g2.drawOval(725, 100, 50,50);
+
+			g2.fillOval(725, 180, 50,50);
+
+			//clay colour
+			g.setColor(Color.decode("#D49A89"));
+			g2.drawRect (0, 500, 800, 100);
+			g2.fillRect (0, 500, 800, 100);
+
+			g2.setStroke(new BasicStroke(1));
+			g2.drawOval(650, 20, 50,50);
+
+			g2.setStroke(new BasicStroke(4));
+			g2.drawOval(650, 100, 50,50);
+
+			g2.fillOval(650, 180, 50,50);
+
+			//peach colour
+			g.setColor(Color.decode("#F7D1BA"));
+			g2.drawRect (0, 400, 800, 100);
+			g2.fillRect (0, 400, 800, 100);
+
+			g2.setStroke(new BasicStroke(1));
+			g2.drawOval(575, 20, 50,50);
+
+			g2.setStroke(new BasicStroke(4));
+			g2.drawOval(575, 100, 50,50);
+
+			g2.fillOval(575, 180, 50,50);
+
+			//lines
+			g2.setColor(Color.decode("#F7D1BA"));
+			g2.setStroke(new BasicStroke(3));
+			g2.drawLine(0,380,600,380);
+			g2.setStroke(new BasicStroke(2));
+			g2.drawLine(0,360,500,360);
+			g2.setStroke(new BasicStroke(1));
+			g2.drawLine(0,340,400,340);
+
+
+
 		}
-		
+
 	}	
 }
